299af04724a67ae5e2af6fca2b2f34ef7b9b3e38
chiziivictor <chizivwokoma@gmail.com>
2022-07-13 23:57:30 +0100

    modify values prop

--- Forms/FormikControl.tsx
+++ Forms/FormikControl.tsx
@@ -1,24 +1,24 @@
-import React from 'react';
-import SelectInput, { OptionProps, valuesProps } from './SelectInput';
-import Input from './Input';
+import React from "react";
+import SelectInput, { OptionProps } from "./SelectInput";
+import Input from "./Input";
 
 interface FormikControlProps {
   placeholder?: string;
   name: string;
   control: string;
   type?: string;
-  options: OptionProps[];
-  values: valuesProps;
+  options?: OptionProps[];
+  values: any;
 }
 
 const FormikControl: React.FC<FormikControlProps> = ({ control, ...rest }) => {
   switch (control) {
-    case 'input':
+    case "input":
       return <Input {...rest} />;
-    case 'textarea':
-    case 'select':
+    case "textarea":
+    case "select":
       return <SelectInput {...rest} />;
-    case 'radio':
+    case "radio":
     default:
       return null;
   }
